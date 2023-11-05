6473edfda0000235043f80cdc91d4f4b9d89c877
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-05 14:41:56 +0530

    new file:   app/Components/Search.jsx 	modified:   app/globals.css 	new file:   app/images/Icon.png 	new file:   app/images/header.png 	new file:   app/images/jon-tyson-A-obUh61bKw-unsplash.jpg 	new file:   app/images/plus.png 	new file:   app/images/remove.png 	new file:   app/images/star.png 	modified:   app/page.tsx 	new file:   app/watchlist/page.tsx 	modified:   package-lock.json 	modified:   package.json

---
+++ app/Components/Search.jsx
@@ -0,0 +1,90 @@
+'use client'
+import React from "react";
+import plus from "../images/plus.png"
+import star from "../images/star.png"
+import Link from "next/link";
+
+export default function Search(){
+    const [ query, setQuery ] = React.useState('')
+    const [ results, setResults ] = React.useState('')
+    const [ render, setRender ] = React.useState('')
+
+    React.useEffect(function(){
+    const movieList = document.getElementById('movie-results')
+    movieList.innerHTML = ''
+    const query = document.getElementById("query").value
+    let searchItems = []
+    fetch(`https://www.omdbapi.com/?s=${query}&apikey=37bc2f93`)
+        .then(res => res.json())
+        .then(data => {
+            searchItems = (data.Search ? data.Search.map(i => i.imdbID) : '')
+            for (let id in searchItems){
+                let result = {}
+                fetch(`https://www.omdbapi.com/?i=${searchItems[id]}&apikey=37bc2f93`)
+                    .then(res => res.json())
+                    .then(data => {
+                        movieList.innerHTML += `
+                        <div id='item-card'>
+                            <img class='poster' src='${data.Poster}'>
+                            <div class='movie-desc'>
+                                <div class='title-rating'>
+                                    <h3>${data.Title}</h3>
+                                    <div class='flex'>
+                                        <img src='${star.src}'>
+                                        <p>${data.imdbRating}</p>
+                                    </div>
+                                </div>
+                                <div class='info'>
+                                    <p>${data.Runtime}</p>
+                                    <p class='align'>${data.Genre}</p>
+                                    <div id='add' data-imdbID='${data.imdbID}' class='flex'>
+                                        <img id='add' data-imdbID='${data.imdbID}' src='${plus.src}'>
+                                        <p id='add' data-imdbID='${data.imdbID}'>Watchlist</p>
+                                    </div>
+                                </div>
+                                <p>${data.Plot}</p>
+                            <div>
+                        </div>
+                        `
+                        }
+                        
+                    )
+                    
+            }
+    })
+    }, [ query ])
+
+    function temp(e){
+        if (e.target.id == 'add'){
+            if (localStorage.getItem("movies") === ["", ""]){
+            localStorage.removeItem('movies')
+            console.log(localStorage.getItem("movies"))
+
+            }
+            else if (localStorage.getItem("movies") === null){
+                localStorage.setItem("movies", `${e.target.dataset.imdbid} `)
+                console.log(localStorage.getItem("movies"))
+            } else {
+                let arr = localStorage.getItem("movies")
+                localStorage.removeItem("movies")
+                arr += `${e.target.dataset.imdbid} `
+                localStorage.setItem('movies', `${arr}`)
+                console.log(localStorage.getItem("movies"))
+            }
+        }
+    }
+
+    return (
+    <div onClick={(e) => temp(e)}>
+          <div id='nav'>
+              <div id='header'>
+                  <h1 id='title'>Find Your Film</h1>
+                  <Link href='./watchlist'>My Watchlist</Link>
+              </div>
+                  <input id='query' className="search" type='text' placeholder="Search for a movie" onChange={e => setQuery(e.target.value)}></input>
+              
+          </div>
+          <section className="movie-results" id='movie-results'>{results ? JSON.stringify(results.Search) : null}</section>
+    </div>
+    )
+}
---
+++ app/watchlist/page.tsx
@@ -0,0 +1,98 @@
+'use client'
+import React from 'react'
+import star from '../images/star.png'
+import remove from '../images/remove.png'
+
+console.log(star.src)
+
+export default function Watchlist(){
+const [ list, setList ] = React.useState('')
+React.useEffect(function(){
+    setList(localStorage.getItem('movies') ? localStorage.getItem('movies').split(' ').slice(0, list.length - 1) : "")
+
+}, [ ])
+
+const movieList = document.getElementById('movie-results')
+
+const card = (data) => `
+                <div id='item-card'>
+                            <img class='poster' src='${data.Poster}'>
+                            <div class='movie-desc'>
+                                <div class='title-rating'>
+                                    <h3>${data.Title}</h3>
+                                    <div class='flex'>
+                                        <img src='${star.src}'>
+                                        <p>${data.imdbRating}</p>
+                                    </div>
+                                </div>
+                                <div class='info'>
+                                    <p>${data.Runtime}</p>
+                                    <p class='align'>${data.Genre}</p>
+                                    <div id='add' data-imdbID='${data.imdbID}' class='flex'>
+                                        <img id='add' data-imdbID='${data.imdbID}' src='${remove.src}'>
+                                        <p id='add' data-imdbID='${data.imdbID}'>Remove</p>
+                                    </div>
+                                </div>
+                                <p>${data.Plot}</p>
+                            <div>
+                        </div>
+                `
+
+
+React.useEffect(function(){
+
+    for ( let id in list ){
+        console.log(list)
+        fetch(`https://www.omdbapi.com/?i=${list[id]}&apikey=37bc2f93`)
+        .then(res => res.json())
+        .then(data => 
+            movieList ? movieList.innerHTML +=  card(data) :  'chit'
+        )
+    }
+}, [list])
+
+
+
+function whatever(e){
+
+    if (e.target.dataset.imdbid){
+        console.log(`starting val: ${localStorage.getItem('movies')}`)
+        let str = localStorage.getItem('movies').split(' ')
+        let arr = str.slice(0, str.length - 1)
+        let index = arr.indexOf(e.target.dataset.imdbid)
+        arr = arr.filter(i => i !== e.target.dataset.imdbid)
+        console.log(arr)
+        localStorage.setItem('movies', `${arr.join(' ')} `)
+        console.log(arr)
+        for ( let id in arr ){
+            console.log(arr)
+            movieList.innerHTML = ''
+            fetch(`https://www.omdbapi.com/?i=${arr[id]}&apikey=37bc2f93`)
+                .then(res => res.json())
+                .then(data => 
+                    movieList.innerHTML += card(data)
+                )
+}
+        if (arr.length = 0){
+            movieList.innerHTML = ''            
+        }
+        if (localStorage.getItem('movies') == " "){
+            localStorage.removeItem('movies')
+            movieList.innerHTML = ''
+        }
+        console.log(`ending val: ${localStorage.getItem('movies')}`)
+    }
+}
+
+//  localStorage.removeItem('movies')
+    return (
+        <div>
+            <div id='header'>
+                <h1 id='title'>Find Your Film</h1>
+                <p><a href='./'>Search for movies</a></p>
+            </div>
+            <div id='movie-results' className='movie-results watchlist' onClick={e => whatever(e)}>
+            </div>
+        </div>
+    )
+}
